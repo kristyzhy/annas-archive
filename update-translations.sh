64d33140433dff0879afac813d12f6d636e0678c
AnnaArchivist <1-AnnaArchivist@users.noreply.annas-software.org>
2023-02-28 00:00:00 +0300

    Set no wrap for .po files

--- update-translations.sh
+++ update-translations.sh
@@ -6,9 +6,9 @@ set -Eeuxo pipefail
 pybabel extract --omit-header -F babel.cfg -o messages.pot .
 pybabel extract --omit-header -F babel.cfg -o messages.pot .
 pybabel extract --omit-header -F babel.cfg -o messages.pot .
-pybabel update --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
-pybabel update --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
-pybabel update --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
+pybabel update --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
+pybabel update --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
+pybabel update --no-wrap --omit-header -i messages.pot -d allthethings/translations --no-fuzzy-matching
 pybabel compile -f -d allthethings/translations
 pybabel compile -f -d allthethings/translations
 pybabel compile -f -d allthethings/translations
