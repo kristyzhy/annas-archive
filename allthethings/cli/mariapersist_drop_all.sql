1521672e14e8f9bcb1077d385bffeb0cc1482f9c
dfs8h3m <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2023-04-04 00:00:00 +0300

    Mariadb replica

--- allthethings/cli/mariapersist_drop_all.sql
+++ allthethings/cli/mariapersist_drop_all.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS `mariapersist_accounts`;
+DROP TABLE IF EXISTS `mariapersist_account_logins`;
 DROP TABLE IF EXISTS `mariapersist_downloads`;
 DROP TABLE IF EXISTS `mariapersist_downloads_hourly`;
 DROP TABLE IF EXISTS `mariapersist_downloads_hourly_by_ip`;
---
+++ mariapersistreplica-conf/init.sql
@@ -0,0 +1,2 @@
+GRANT REPLICATION SLAVE ADMIN ON *.* TO 'mariapersist'@'%';
+GRANT REPLICA MONITOR ON *.* TO 'mariapersist'@'%';
