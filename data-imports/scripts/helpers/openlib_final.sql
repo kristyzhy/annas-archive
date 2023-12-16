196b668685bb0521e843663ffc534b145d7f9141
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2023-12-16 00:00:00 +0000

    zzz

--- data-imports/scripts/helpers/openlib_final.sql
+++ data-imports/scripts/helpers/openlib_final.sql
@@ -42,6 +42,7 @@ ALTER TABLE allthethings.ol_base ADD PRIMARY KEY(ol_key);
 
 -- Note that many books have only ISBN10.
 -- ~20mins
+DROP TABLE IF EXISTS allthethings.ol_isbn13;
 CREATE TABLE allthethings.ol_isbn13 (isbn CHAR(13), ol_key CHAR(200), PRIMARY KEY(isbn, ol_key)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin IGNORE SELECT x.isbn AS isbn, ol_key FROM allthethings.ol_base b CROSS JOIN JSON_TABLE(b.json, '$.isbn_13[*]' COLUMNS (isbn CHAR(13) PATH '$')) x WHERE ol_key LIKE '/books/OL%' AND LENGTH(x.isbn) = 13 AND x.isbn REGEXP '[0-9]{12}[0-9X]';
 -- ~60mins
 INSERT IGNORE INTO allthethings.ol_isbn13 (isbn, ol_key) SELECT ISBN10to13(x.isbn) AS isbn, ol_key FROM allthethings.ol_base b CROSS JOIN JSON_TABLE(b.json, '$.isbn_10[*]' COLUMNS (isbn CHAR(10) PATH '$')) x WHERE ol_key LIKE '/books/OL%' AND LENGTH(x.isbn) = 10 AND x.isbn REGEXP '[0-9]{9}[0-9X]';
