8aeeba566024b8f4f4065aa5336e28ba44d4fd6f
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-04-02 00:00:00 +0000

    zzz

--- data-imports/scripts/load_aac_duxiu_records.sh
+++ data-imports/scripts/load_aac_duxiu_records.sh
@@ -9,3 +9,5 @@ set -Eeuxo pipefail
 cd /temp-dir/aac_duxiu_records
 
 PYTHONIOENCODING=UTF8:ignore python3 /scripts/helpers/load_aac.py /temp-dir/aac/annas_archive_meta__aacid__duxiu_records*
+
+echo 'CREATE TABLE annas_archive_meta__aacid__duxiu_records_by_filename_decoded (aacid VARCHAR(250) NOT NULL, filename_decoded VARCHAR(8000) NOT NULL, PRIMARY KEY(aacid), INDEX filename_decoded (filename_decoded(100))) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin SELECT aacid, JSON_EXTRACT(metadata, "$.record.filename_decoded") AS filename_decoded FROM annas_archive_meta__aacid__duxiu_records WHERE JSON_EXTRACT(metadata, "$.record.filename_decoded") IS NOT NULL;' | mariadb -h aa-data-import--mariadb -u root -ppassword --show-warnings -vv
