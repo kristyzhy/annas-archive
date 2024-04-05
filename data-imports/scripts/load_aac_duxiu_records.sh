0700dca2267337befc44df231fa0fa082efd6bf1
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-04-05 00:00:00 +0000

    zzz

--- data-imports/scripts/load_aac_duxiu_records.sh
+++ data-imports/scripts/load_aac_duxiu_records.sh
@@ -6,9 +6,7 @@ set -Eeuxo pipefail
 # Feel free to comment out steps in order to retry failed parts of this script, when necessary.
 # Load scripts are idempotent, and can be rerun without losing too much work.
 
-cd /temp-dir/aac_duxiu_records
-
-PYTHONIOENCODING=UTF8:ignore python3 /scripts/helpers/load_aac.py /temp-dir/aac/annas_archive_meta__aacid__duxiu_records*
+PYTHONIOENCODING=UTF8:ignore python3 /scripts/helpers/load_aac.py /temp-dir/aac_duxiu_records/annas_archive_meta__aacid__duxiu_records*
 
 # echo 'CREATE TABLE annas_archive_meta__aacid__duxiu_records_by_filename_decoded (aacid VARCHAR(250) NOT NULL, filename_decoded VARCHAR(8000) NOT NULL, PRIMARY KEY(aacid), INDEX filename_decoded (filename_decoded(100))) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin SELECT aacid, JSON_EXTRACT(metadata, "$.record.filename_decoded") AS filename_decoded FROM annas_archive_meta__aacid__duxiu_records WHERE JSON_EXTRACT(metadata, "$.record.filename_decoded") IS NOT NULL;' | mariadb -h aa-data-import--mariadb -u root -ppassword --show-warnings -vv
 
--- data-imports/scripts/load_aac_zlib3_records.sh
+++ data-imports/scripts/load_aac_zlib3_records.sh
@@ -6,6 +6,4 @@ set -Eeuxo pipefail
 # Feel free to comment out steps in order to retry failed parts of this script, when necessary.
 # Load scripts are idempotent, and can be rerun without losing too much work.
 
-cd /temp-dir/aac_zlib3_records
-
-PYTHONIOENCODING=UTF8:ignore python3 /scripts/helpers/load_aac.py /temp-dir/aac/annas_archive_meta__aacid__zlib3_records*
+PYTHONIOENCODING=UTF8:ignore python3 /scripts/helpers/load_aac.py /temp-dir/aac_zlib3_records/annas_archive_meta__aacid__zlib3_records*
