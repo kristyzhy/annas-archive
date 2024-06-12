aada91557490200a745bc8c78ef11eb0b2acde33
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-06-12 00:00:00 +0000

    zzz

--- data-imports/scripts/dump_elasticsearch.sh
+++ data-imports/scripts/dump_elasticsearch.sh
@@ -11,7 +11,8 @@ cd /exports
 rm -rf /exports/elasticsearch
 mkdir /exports/elasticsearch
 # https://github.com/elasticsearch-dump/elasticsearch-dump/issues/651#issuecomment-564545317
-NODE_OPTIONS="--max-old-space-size=16384" multielasticdump --input=${ELASTICSEARCH_HOST:-http://elasticsearch:9200} --output=/exports/elasticsearch --match='aarecords.*' --parallel=32 --limit=10000 --fsCompress --includeType=data,mapping,analyzer,alias,settings,template
+export NODE_OPTIONS="--max-old-space-size=16384"
+multielasticdump --input=${ELASTICSEARCH_HOST:-http://elasticsearch:9200} --output=/exports/elasticsearch --match='aarecords.*' --parallel=16 --limit=10000 --fsCompress --includeType=data,mapping,analyzer,alias,settings,template
 # WARNING: multielasticdump doesn't properly handle children getting out of memory errors.
 # Check valid gzips as a workaround. Still somewhat fragile though!
 zcat /exports/elasticsearch/*.json.gz | wc -l
--- data-imports/scripts/dump_elasticsearchaux.sh
+++ data-imports/scripts/dump_elasticsearchaux.sh
@@ -11,7 +11,8 @@ cd /exports
 rm -rf /exports/elasticsearchaux
 mkdir /exports/elasticsearchaux
 # https://github.com/elasticsearch-dump/elasticsearch-dump/issues/651#issuecomment-564545317
-NODE_OPTIONS="--max-old-space-size=16384" multielasticdump --input=${ELASTICSEARCHAUX_HOST:-http://elasticsearchaux:9201} --output=/exports/elasticsearchaux --match='aarecords.*' --parallel=32 --limit=10000 --fsCompress --includeType=data,mapping,analyzer,alias,settings,template
+export NODE_OPTIONS="--max-old-space-size=16384"
+multielasticdump --input=${ELASTICSEARCHAUX_HOST:-http://elasticsearchaux:9201} --output=/exports/elasticsearchaux --match='aarecords.*' --parallel=16 --limit=10000 --fsCompress --includeType=data,mapping,analyzer,alias,settings,template
 # WARNING: multielasticdump doesn't properly handle children getting out of memory errors.
 # Check valid gzips as a workaround. Still somewhat fragile though!
 zcat /exports/elasticsearchaux/*.json.gz | wc -l
