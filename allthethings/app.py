0e91c61e3d13f364bd129ac0c0632738a04a2fdd
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-07-17 00:00:00 +0000

    zzz

--- allthethings/app.py
+++ allthethings/app.py
@@ -242,7 +242,9 @@ def extensions(app):
         doc_counts = {content_type['key']: content_type['doc_count'] for content_type in all_search_aggs('en', 'aarecords')[0]['search_content_type']}
         doc_counts_journals = {content_type['key']: content_type['doc_count'] for content_type in all_search_aggs('en', 'aarecords_journals')[0]['search_content_type']}
         doc_counts['total_without_journals'] = sum(doc_counts.values())
-        doc_counts['journal_article'] = doc_counts_journals.get('journal_article') or 0
+        # doc_counts['journal_article'] = doc_counts_journals.get('journal_article') or 0
+        # TODO:TEMPFIX remove temporary fix number
+        doc_counts['journal_article'] = 100357126
         doc_counts['total'] = doc_counts['total_without_journals'] + doc_counts['journal_article']
         doc_counts['book_comic'] = doc_counts.get('book_comic') or 0
         doc_counts['magazine'] = doc_counts.get('magazine') or 0
